b46b4033ddccd56f8ba49d465db26755d40e9c21
rafael-puelma <rafael.puelma@outlook.com>
2020-10-16 14:13:40 +0200

    uppdatering

--- todolist.js
+++ todolist.js
@@ -1,25 +1,17 @@
 console.log("hej");
 
 const addTodoForm = document.querySelector("#addTodoForm");
-// console.log(addTodoForm);
-
 const addTodoInput = document.querySelector("#addTodoInputField");
-
 const todoList = document.querySelector("#todoList");
-// focus on text input
-//document.getElementById("myText").focus();
-
 addTodoInput.focus();
-// inte ""document.getElementById("myText").focus();"" eftersom vi ahr redan functionen addtodo
 
-// add event listenr for submitting form.
 addTodoForm.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log("form is submitted");
-  // console.log(addTodoInput.value);
+
   addTodo(addTodoInput.value);
 });
-// funktionen lägger till ett item till på listan
+
 function addTodo(todoText) {
   todoList.insertAdjacentHTML(
     "beforeend",
@@ -32,14 +24,3 @@ function deleteTodo(element) {
   element.parentElement.remove();
   addTodoInput.focus();
 }
-
-// implementing daddEventListener("click", function(){
-// add event listenr for submitting form.
-// document.getElementById("todoList").addEventListener("click", function(){
-// console.log("det gick")
-// });
-
-// dedta är först inlämings uppgiften
-
-// uppgift 2 är miniräknare
-// använd function eval
