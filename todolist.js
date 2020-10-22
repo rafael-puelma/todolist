e69696bcff88cac45bc6ee4e0dac7fe1471cf4e7
rafael-puelma <rafael.puelma@outlook.com>
2020-10-22 13:16:51 +0200

    v3

--- todolist.js
+++ todolist.js
@@ -7,15 +7,16 @@ addTodoForm.addEventListener("submit", (event) => {
   event.preventDefault();
   addTodo(addTodoInput.value);
 });
-
+// skappar en ny rad i HTML där vi lägger vår nya todo
 function addTodo(todoText) {
   todoList.insertAdjacentHTML(
     "beforeend",
     `<li>${todoText}<button onclick="deleteTodo(this)">Delete</button></li>`
   );
-
+  // resaradd todo rutan efter att vi har lagt till ettt nytt todo
   addTodoForm.reset();
 }
+// tar bort todo items från listan
 function deleteTodo(element) {
   element.parentElement.remove();
   addTodoInput.focus();
